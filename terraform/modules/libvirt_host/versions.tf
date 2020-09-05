000cfc0c4a5514adaa0e1935bbcba45e6c6b244f
Dionysis Grigoropoulos <dgrig@erethon.com>
2020-09-05 20:53:25 +0300

    modules: Ping libvirt provider version

---
+++ terraform/modules/libvirt_host/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source = "erethon.com/third-party/libvirt"
+      version = "0.6.2"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  required_version = ">= 0.13"
+}
