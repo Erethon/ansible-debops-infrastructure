11476bedbdcbb5974013af4d2e2fd73f9066308c
Dionysis Grigoropoulos <dgrig@erethon.com>
2023-11-07 20:35:04 +0200

    k3s-home: Init HomeAssistant namespace and pod

---
+++ terraform/infrastructure/k3s-home/ha.tf
@@ -0,0 +1,38 @@
+resource "kubernetes_namespace" "ha" {
+  metadata {
+    name = "homeassistant"
+  }
+}
+
+resource "kubernetes_pod" "ha" {
+  metadata {
+    name      = "homeassistant"
+    namespace = kubernetes_namespace.ha.id
+  }
+
+  spec {
+    container {
+      image = "ghcr.io/home-assistant/home-assistant"
+      name  = "homeassistant"
+
+      port {
+        container_port = 8123
+        host_port      = 8123
+      }
+      security_context {
+        privileged = true
+      }
+      volume_mount {
+        mount_path = "/config"
+        name       = "haconfig"
+        sub_path   = "config"
+      }
+    }
+    volume {
+      name = "haconfig"
+      host_path {
+        path = "/volumes/ha"
+      }
+    }
+  }
+}
