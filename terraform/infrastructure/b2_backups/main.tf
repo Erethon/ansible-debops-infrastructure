b53381ae1fbc860134b133f6be57beae838b17b1
Dionysis Grigoropoulos <dgrig@erethon.com>
2023-12-05 18:45:28 +0200

    b2: Add bucket for k3s1 k8s backups

--- terraform/infrastructure/b2_backups/main.tf
+++ terraform/infrastructure/b2_backups/main.tf
@@ -19,3 +19,14 @@ resource "b2_application_key" "plothopes_db" {
   key_name = "plothopes-db-key"
   bucket_id = b2_bucket.plothopes_db.bucket_id
 }
+
+resource "b2_bucket" "home_k3s" {
+  bucket_name = "home-k3s"
+  bucket_type = "allPrivate"
+}
+
+resource "b2_application_key" "home_k3s" {
+  capabilities = ["writeFiles", "readFiles", "listFiles"]
+  key_name = "home-k3s"
+  bucket_id = b2_bucket.home_k3s.bucket_id
+}
